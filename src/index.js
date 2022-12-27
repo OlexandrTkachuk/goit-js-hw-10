85e970e2c520e04616f51eff5977edfa17fe6538
Olexandr Tkachuk <temishena@gmail.com>
2022-12-27 22:35:50 +0200

    start hw10

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+const DEBOUNCE_DELAY = 300;
+
+//  <input type="text" id="search-box" />
+//     <ul class="country-list"></ul>
+//     <div class="country-info"></div>
+
